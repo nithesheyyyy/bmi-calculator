c290db625cc484e451387f8cb1e396d255a7b99f
Nithesh Kumar <cnksandyff@gmail.com>
2026-01-28 11:36:50 +0530

    add project files

---
+++ 720723110083/src/main/java/com/bmi/App.java
@@ -0,0 +1,31 @@
+package com.bmi;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter weight in kg: ");
+        double weight = sc.nextDouble();
+
+        System.out.print("Enter height in meters: ");
+        double height = sc.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        System.out.println("Your BMI is: " + bmi);
+
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi < 25) {
+            System.out.println("Normal weight");
+        } else if (bmi < 30) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+
+        sc.close();
+    }
+}
---
+++ 720723110083/src/test/java/com/bmi/AppTest.java
@@ -0,0 +1,13 @@
+package com.bmi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
